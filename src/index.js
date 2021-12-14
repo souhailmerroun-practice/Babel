e262f231e866414d6f9f0d11a59ec1463b5e517d
Souhail Merroun <souhail.merroun1996@gmail.com>
2021-12-14 14:03:27 +0100

    Test Babel

---
+++ src/index.js
@@ -0,0 +1,14 @@
+console.log('hi');
+
+const PI = 3.141593;
+
+class Shape {
+    constructor (id, x, y) {
+        this.id = id
+        this.move(x, y)
+    }
+    move (x, y) {
+        this.x = x
+        this.y = y
+    }
+}
